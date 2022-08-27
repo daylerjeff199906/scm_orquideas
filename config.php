cd8688230b0f7b80d4decdb52757e5d7657beadf
Dayler <daylersan@gmail.com>
2022-08-27 00:18:27 -0500

    cambio2

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 		
-		$mysqli = new mysqli('localhost', 'root', '', 'scm_orquideas');
+		$mysqli = new mysqli('localhost', 'root', 'Jarvis@3000', 'scm_orquideas');
 		if ($mysqli->connect_errno) {
 			echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 		}
